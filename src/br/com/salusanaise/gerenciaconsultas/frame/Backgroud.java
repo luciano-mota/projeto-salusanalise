7eabcad257ded741f83a26d59a426a20214dd73f
Luciano Mota <amoosenhorjesus3@gmail.com>
2020-06-15 09:43:09 -0300

    Background

---
+++ src/br/com/salusanaise/gerenciaconsultas/frame/Backgroud.java
@@ -0,0 +1,50 @@
+package br.com.salusanaise.gerenciaconsultas.frame;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+public class Backgroud {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Backgroud window = new Backgroud();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Backgroud() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(0, 12, 450, 249);
+		frame.getContentPane().add(lblNewLabel);
+	}
+
+}
